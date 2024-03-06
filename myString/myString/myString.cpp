f0750a808bc7c241047a4b26b0ef6ef346e78830
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-03-06 14:21:22 +0100

    string class done

--- myString/myString/myString.cpp
+++ myString/myString/myString.cpp
@@ -1,1 +1,12 @@
+#include "myString.hpp"
+#include "string.h"
+#include <iostream>
 
+int main() {
+    myString helloWorld("Hello", 100);
+    helloWorld.append(", World\n");
+    helloWorld.append("Alli");
+    helloWorld.append("hopa!");
+
+    helloWorld.print();
+}
