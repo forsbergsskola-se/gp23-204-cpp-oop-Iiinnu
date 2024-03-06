0089e0624e994e50668d8c2ef40dbbc9d66eb161
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-03-06 12:38:36 +0100

    Created a new string project and added a gitignore

---
+++ String/String/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  String
+//
+//  Created by Unnie Jansson on 2024-03-06.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
