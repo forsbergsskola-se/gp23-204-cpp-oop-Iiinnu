061150f98e55fa2117ec613165046dd62f8c6c3f
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-03-07 12:40:52 +0100

    trying to move more to the cpp file, not fully understanding how i write the definition for the functions in the cpp file instead

--- myString/myString/myString.cpp
+++ myString/myString/myString.cpp
@@ -2,6 +2,16 @@
 #include "string.h"
 #include <iostream>
 
+
+using namespace std;
+
+myString::myString(const int maxSize): maxSize(maxSize), buffer(new char[maxSize])
+{
+    
+    
+}
+
+
 int main() {
     myString helloWorld("Hello", 100);
     helloWorld.append(", World\n");
@@ -10,3 +20,130 @@ int main() {
 
     helloWorld.print();
 }
+
+
+myString(const int maxSize) :maxSize(maxSize)
+{
+buffer = new char[maxSize];
+buffer[length] = '\0';
+
+}
+
+
+myString(const char* text, const int maxSize) : maxSize(maxSize)
+{
+cout << "Is it alive?!\n";
+buffer = new char[maxSize];
+while (text[length] != '\0' && length < maxSize)
+{
+buffer[length] = text[length];
+length++;
+}
+}
+
+~myString() {
+delete[] buffer;
+cout << "\nBye Bye\n";
+}
+
+void append(const char* text1)
+{
+int textLength = 0;
+while (text1[textLength] != '\0')
+{
+textLength++;
+}
+if (length + textLength < maxSize)
+{
+for (int i = 0; i < textLength; i++)
+{
+buffer[length] = text1[i];
+length++;
+}
+buffer[length] = '\0';
+}
+cout << "Added the append word to the buffer\n";
+}
+
+void appendLine(const char* text2)
+{
+int textLength = 0;
+while (text2[textLength] != '\0')
+{
+textLength++;
+}
+if (length + textLength < maxSize)
+{
+for (int i = 0; i < textLength; i++)
+{
+buffer[length] = text2[i];
+length++;
+}
+buffer[length] = '\n';
+length++;
+buffer[length] = '\0';
+}
+cout << "Added the appendLine word to the buffer\n";
+}
+
+void Replace(char replace, char replacer)
+{
+int m = 0;
+while (buffer[m] != '\0')
+{
+if (buffer[m] == replace)
+{
+buffer[m] = replacer;
+}
+m++;
+if (m >= length)
+{
+buffer[m] = '\0';
+}
+}
+}
+
+void print()
+{
+cout << "Printing stuff\n";
+cout << buffer;
+}
+
+char* getString()
+{
+return buffer;
+}
+
+
+bool operator== (const myString& other) const
+{
+int i = 0;
+while (buffer[i] != '\0' && other.buffer[i] != '\0')
+{
+if (buffer[i] != other.buffer[i])
+{
+return false;
+}
+i++;
+}
+return buffer[i] == other.buffer[i];
+}
+
+int indexOf(const myString& other) const
+{
+int i = 0;
+int j = 0;
+while (buffer[i] != '\0' && other.buffer[j] != '\0')
+{
+if (buffer[i] == other.buffer[j])
+{
+j++;
+return i;
+}
+i++;
+}
+return -1;
+}
+
+};
+
