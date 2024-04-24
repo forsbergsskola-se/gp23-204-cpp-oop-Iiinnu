85749c276defee603aded83be66099262032620d
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-04-24 14:01:43 +0200

    tried to add google test , not working they way i want

---
+++ myString/MainTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MainTest
+//
+//  Created by Unnie Jansson on 2024-04-24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
